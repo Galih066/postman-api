c55ef5fddd36b6bcacd0ed1e844788ddb7375f47
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2025-07-02 11:41:31 +0800

    fix(Expanses): format date value when add expanses

--- src/Services/Expanses/daily.services.ts
+++ src/Services/Expanses/daily.services.ts
@@ -1,12 +1,31 @@
+import moment from "moment";
 import { DailyExpnsIntfc } from "../../Interfaces/expanses.interface.js";
 import { InternalServerError, ApiSuccess } from "../../Helpers/response.helper.js";
 import DailyExpanse from "../../Models/daily.model.js";
 
 export const handleDailyExpanses = async (params: DailyExpnsIntfc) => {
     try {
-        const savedData = new DailyExpanse(params);
+        const savedData = new DailyExpanse({ ...params, date: moment(params.date).format('YYYY-MM-DD HH:mm:ss') });
         await savedData.save();
         return ApiSuccess("Success")
+    } catch (error) {
+        console.log(error);
+        return InternalServerError();
+    }
+}
+
+export const getDailyExpanses = async () => {
+    try {
+        const start = moment().startOf("days").format('YYYY-MM-DD HH:mm:ss')
+        const end = moment().endOf("days").format('YYYY-MM-DD HH:mm:ss')
+        const dailyData = await DailyExpanse.find({
+            date: {
+                $gte: new Date(start),
+                $lte: new Date(end)
+            }
+        });
+
+        return ApiSuccess("Success", dailyData);
     } catch (error) {
         console.log(error);
         return InternalServerError();
