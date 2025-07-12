74391186184c06cb1edb887056de8117a17e029a
Galih066 <galihdapaimanda@gmail.com>
2025-07-12 20:55:47 +0800

    fix(Database): add condition for production and development

--- src/Config/database.config.ts
+++ src/Config/database.config.ts
@@ -3,10 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { MONGO_URI } = process.env;
+const { MONGO_URI, NODE_APP, MONGO_URI_PRD } = process.env;
 const connection = async () => {
     try {
-        const connect = await mongoose.connect(String(MONGO_URI));
+        const dbUri = NODE_APP === 'production' ? MONGO_URI_PRD : MONGO_URI;
+        const connect = await mongoose.connect(String(dbUri));
         console.log(`MongoDB connected: ${connect.connection.host}`);
     } catch (error) {
         console.error("MongoDB connection failed:", error);
