d6f2c0bbd9e54090be53e1067fe42b60088ea99e
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2025-07-23 17:47:07 +0800

    fix(Docker): change docker settings

--- src/Config/database.config.ts
+++ src/Config/database.config.ts
@@ -7,6 +7,8 @@ const { MONGO_URI, NODE_APP, MONGO_URI_PRD } = process.env;
 const connection = async () => {
     try {
         const dbUri = NODE_APP === 'production' ? MONGO_URI_PRD : MONGO_URI;
+        console.log('Node App ', NODE_APP);
+        console.log('URL Connect to ', dbUri);
         const connect = await mongoose.connect(String(dbUri));
         console.log(`MongoDB connected: ${connect.connection.host}`);
     } catch (error) {
