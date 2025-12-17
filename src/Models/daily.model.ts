42fef6bec4ccc0f2ed5a280547e56e2c66e8d023
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2025-12-18 06:57:21 +0700

    add user id

--- src/Models/daily.model.ts
+++ src/Models/daily.model.ts
@@ -3,7 +3,7 @@ const { Schema, model } = mongoose;
 
 const dailyExpanseSchema = new Schema(
     {
-        userId: Number,
+        userId: Schema.Types.ObjectId,
         name: String,
         description: String,
         nominal: Number,
