9b2f4446b4aa2493e58d25ddb5eb7607725299d2
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2025-12-18 14:04:58 +0700

    implement token validation on all route

--- src/Routes/expanses.route.ts
+++ src/Routes/expanses.route.ts
@@ -4,6 +4,7 @@ import * as TypeController from "../Controllers/Expanses/type.controller.js";
 import * as FreqController from "../Controllers/Expanses/frequence.controller.js";
 import * as IncomeController from "../Controllers/Expanses/income.controller.js";
 import validation from "../Middlewares/validation.middleware.js";
+import tokenValidation from "../Middlewares/auth.middleware.js";
 import {
     addExpanses,
     dailyExpanses,
@@ -16,19 +17,19 @@ import {
 
 const router = Router();
 
-router.post('/add', validation(addExpanses, 'body'), ExpansesController.addDaily);
-router.get('/get-daily', validation(dailyExpanses, 'query'), ExpansesController.getDaily);
-router.post('/add-type', validation(addType, 'body'), TypeController.addType);
-router.get('/get-type', TypeController.getType);
-router.get('/get-freq', FreqController.allFreq);
-router.post('/add-freq', validation(addFreq, 'body'), FreqController.addFreq);
-router.get('/get-summary', validation(dailyExpanses, 'query'), ExpansesController.getSummary);
-router.post('/add-income', validation(addInc, 'body'), IncomeController.addUserIncome);
-router.get('/get-income', validation(getInc, 'query'), IncomeController.getUserIncome);
-router.get('/daily-chart', validation(getInc, 'query'), ExpansesController.dailyChart);
-router.get('/summary-analythic', validation(getInc, 'query'), ExpansesController.summAnalythic);
-router.get('/list', validation(expansesList, 'query'), ExpansesController.dailyList);
-router.get('/monthly-summary', ExpansesController.monthlySummary);
-router.get('/get-non-daily', validation(dailyExpanses, 'query'), ExpansesController.getNonDaily);
+router.post('/add', [tokenValidation, validation(addExpanses, 'body')], ExpansesController.addDaily);
+router.get('/get-daily', [tokenValidation, validation(dailyExpanses, 'query')], ExpansesController.getDaily);
+router.post('/add-type', [tokenValidation, validation(addType, 'body')], TypeController.addType);
+router.get('/get-type', tokenValidation, TypeController.getType);
+router.get('/get-freq', tokenValidation, FreqController.allFreq);
+router.post('/add-freq', [tokenValidation, validation(addFreq, 'body')], FreqController.addFreq);
+router.get('/get-summary', [tokenValidation, validation(dailyExpanses, 'query')], ExpansesController.getSummary);
+router.post('/add-income', [tokenValidation, validation(addInc, 'body')], IncomeController.addUserIncome);
+router.get('/get-income', [tokenValidation, validation(getInc, 'query')], IncomeController.getUserIncome);
+router.get('/daily-chart', [tokenValidation, validation(getInc, 'query')], ExpansesController.dailyChart);
+router.get('/summary-analythic', [tokenValidation, validation(getInc, 'query')], ExpansesController.summAnalythic);
+router.get('/list', [tokenValidation, validation(expansesList, 'query')], ExpansesController.dailyList);
+router.get('/monthly-summary', tokenValidation, ExpansesController.monthlySummary);
+router.get('/get-non-daily', [tokenValidation, validation(dailyExpanses, 'query')], ExpansesController.getNonDaily);
 
 export default router;
