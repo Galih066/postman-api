0ddce51404c99c397dfb31e901e756d7e9a9b94f
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2025-12-19 08:30:47 +0700

    add helper for toke process

--- src/Helpers/string.helper.ts
+++ src/Helpers/string.helper.ts
@@ -1,1 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const { SECRET_KEY } = process.env;
+
 export const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1)
+
+export const generatingToken = (data: any) => {
+    const token = jwt.sign(data, String(SECRET_KEY));
+    return token as string
+}
+
+export const decodingToken = (token: string) => {
+    try {
+        const decode = jwt.verify(token, String(SECRET_KEY)) as { context: string, iat: number }
+        return decode.context as string
+    } catch (error) {
+        return null
+    }
+}
