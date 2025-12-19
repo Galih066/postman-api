a5dca99d7eb22073156995b9976af4c04bae7935
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2025-12-19 08:57:31 +0700

    adjustment on logic capitalize helper

--- src/Helpers/string.helper.ts
+++ src/Helpers/string.helper.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const { SECRET_KEY } = process.env;
 
-export const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1)
+export const capitalize = (str: string) => str.replace(/\b\w/g, char => char.toUpperCase())
 
 export const generatingToken = (data: any) => {
     const token = jwt.sign(data, String(SECRET_KEY));
