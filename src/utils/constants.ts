b436d3cfda26b0a5b410915730f183038fc760fb
Galih066 <galihdapaimanda@gmail.com>
2026-01-05 19:55:40 +0800

    feat: add master month ordering

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -14,3 +14,17 @@ export const DEFMONTH: Record<number, string> = {
     11: "November",
     12: "December"
 }
+export const DEFMONTHORDER: Record<string, number> = {
+    "january": 1,
+    "february": 2,
+    "march": 3,
+    "april": 4,
+    "may": 5,
+    "june": 6,
+    "july": 7,
+    "august": 8,
+    "september": 9,
+    "october": 10,
+    "november": 11,
+    "december": 12
+}
